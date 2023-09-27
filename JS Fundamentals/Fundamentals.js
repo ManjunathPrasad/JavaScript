52d6a676dd920fa03022fe38f60b479e4edca864
Code Monkey <manjunathprasad99@gmail.com>
2023-09-27 01:47:19 +0000

    Initial Commit

---
+++ JS Fundamentals/Fundamentals.js
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////
+
+const message = "Hello";
+
+console.log(message);
+//////////////////////////////////////////////////
+//This is a single line comment 
+
+/*  This is 
+    a
+    multi-line
+    comment
+*/
+////////////////////////Global and local scope with let and var//////////////////////////
+let a = 10;
+
+var b = "I'm a string";
+
+/* You can /* nest comments *\/ by escaping slashes */
+
+if ( a ){
+    var x = b;
+}
+
+console.log(x)
+
+//////////////////////////////////////////////////
